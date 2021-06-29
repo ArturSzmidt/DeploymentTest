30b9d15395d69a9ead7c0c0120b68c6e16027e15
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-06-29 16:14:18 +0200

    ++

--- src/services/books/index.js
+++ src/services/books/index.js
@@ -1,5 +1,20 @@
 import express from 'express';
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
 import { getBooks } from '../../lib/fs-tools.js';
+
+const cloudinaryStorage = new CloudinaryStorage({
+  cloudinary, // grab CLOUDINARY_URL from process.env.CLOUDINARY_URL
+  params: {
+    folder: 'books',
+  },
+});
+
+const uploadOnCloudinary = multer({ storage: cloudinaryStorage }).single(
+  'cover'
+);
 
 const booksRouter = express.Router();
 
@@ -7,6 +22,20 @@ booksRouter.get('/', async (req, res, next) => {
   try {
     const books = await getBooks();
     res.send(books);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+booksRouter.post('/', uploadOnCloudinary, async (req, res, next) => {
+  try {
+    console.log(req.file);
+
+    const newBook = { cover: req.file.path };
+
+    // save it in db
+
+    res.send('Created');
   } catch (error) {
     console.log(error);
   }
